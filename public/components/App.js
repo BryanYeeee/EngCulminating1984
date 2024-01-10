acce954a7435f3194085d1caa2d78e4ac050727c
348670050 <haloday873@gmail.com>
2024-01-10 18:14:11 -0500

    initial commit

---
+++ public/components/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "-scroll-ml-6  scroll-smooth snap-mandatory snap-y"
+  }, "TESTETSTETSETETSEE");
+}
+const root = ReactDOM.createRoot(document.querySelector('#app'));
+console.log(root);
+root.render( /*#__PURE__*/React.createElement(App, null));
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+    return (
+        <div className="-scroll-ml-6  scroll-smooth snap-mandatory snap-y">
+            TESTETSTETSETETSEE
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#app'));
+console.log(root)
+root.render(<App />);
