9f0f758df105fc8d713f7846662b3eb4393eed3e
348670050 <haloday873@gmail.com>
2024-01-21 00:27:26 -0500

    Updates

--- components/App.js
+++ components/App.js
@@ -14,7 +14,7 @@ function App(props) {
     className: "flex justify-content gap-6"
   }, /*#__PURE__*/React.createElement("div", {
     className: "w-1/3"
-  }, "Driven by their shared enthusiasm to destroy ", warTarget, ", huge crowds of people surged through the streets in roaring hatred. As the masses marched in unison, thousands of voicescould be heard shouting the barking rhythms of the Hate Song. As the song continued to be sung throughout the day and night,"), /*#__PURE__*/React.createElement("div", {
+  }, "Driven by their shared plusgood enthusiasm to destroy ", warTarget, ", huge crowds of people surged through the streets in roaring hatred. As the masses marched in unison, thousands of voicescould be heard shouting the barking rhythms of the Hate Song. As the song continued to be sung throughout the day and night,"), /*#__PURE__*/React.createElement("div", {
     className: "w-1/3"
   }, "its passion and resonance carried across miles.", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), "Every corner of the city became filled with statues and waxwork displays. Banners adorned with images of Big Brother and were proudly draped across every building setting the perfect environment for Hate Week to happen."), /*#__PURE__*/React.createElement("div", {
     className: "w-1/3 flex flex-col"
@@ -53,7 +53,7 @@ function App(props) {
     className: "flex justify-content gap-6"
   }, /*#__PURE__*/React.createElement("div", {
     className: "w-1/2"
-  }, ' ' + warTarget, "\u2019s orchestrated bombardments leave hospitals, stores, and office buildings completely destroyed. Residential buildings, once home to our many innocent children, now lie in ruin, leaving the air polluted by smoke and flames."), /*#__PURE__*/React.createElement("div", {
+  }, ' ' + warTarget, "\u2019s orchestrated bombardments leave hospitals, stores, and office buildings fullwise destroyed. Residential buildings, once home to our many innocent children, now lie in ruin, leaving the air polluted by smoke and flames."), /*#__PURE__*/React.createElement("div", {
     className: "w-1/2"
   }, warTarget, "\u2019s aggression stands as a testament to their barbarity and utter disregard for the life of Oceania\u2019s citizens. Despite these attacks, the resilience of The Party does not hesitate to fight back. Reports from Minipax reveal the bravery demonstrated by our valiant frontlines.")), /*#__PURE__*/React.createElement("div", null, "They are consistently prevailing in the battle to defend our glorious homeland. The collective will of our people, guided by the wisdom of Big Brother, remains an unstoppable force against ", warTarget, "\u2019s attempts to challenge the strength of our great country."))), /*#__PURE__*/React.createElement("div", {
     className: "border-2 border-black text-sm p-2 my-6"
@@ -70,7 +70,7 @@ function App(props) {
     className: "subtitle"
   }, "As nighttime struck, crowds of people reportedly filled up one of the central London squares. "), /*#__PURE__*/React.createElement("div", {
     className: "flex justify-content gap-8"
-  }, /*#__PURE__*/React.createElement("div", null, "The sky was lit up by mesmerizing scarlet banners, each displaying the faces of our ", warTarget + "n", " enemies. Trumpets were blaring in the background alongside the rolling of drums and stomping feet."), /*#__PURE__*/React.createElement("div", null, "Sounds of planes, tanks, and guns filled the city as they stormed in around the sea of several thousand faces. The Junior Spies joined in, fearlessly asserting their voices, cutting through the air with an unmatched intensity."), /*#__PURE__*/React.createElement("div", null, "In a remarkable display of youthful determination, their hateful expressions truly stood out, highlighting the passion and strength behind Oceania\u2019s next generation.", ' ' + warTarget, " will get what they deserve for their disrespect towards Big Brother, with the voices of the public proving it to be true.")), /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React.createElement("div", null, "The sky was lit up by mesmerizing scarlet banners, each displaying the faces of our plusungood ", warTarget + "n", " enemies. Trumpets were blaring in the background alongside the rolling of drums and stomping feet."), /*#__PURE__*/React.createElement("div", null, "Sounds of planes, tanks, and guns filled the city as they stormed in around the sea of several thousand faces. The Junior Spies joined in, fearlessly asserting their voices, cutting through the air with an unmatched intensity."), /*#__PURE__*/React.createElement("div", null, "In a remarkable display of youthful determination, their hateful expressions truly stood out, highlighting the passion and strength behind Oceania\u2019s next generation.", ' ' + warTarget, " will get what they deserve for their disrespect towards Big Brother, with the voices of the public proving it to be true.")), /*#__PURE__*/React.createElement("div", {
     className: "subtitle"
   }, "The great orator of our Inner Party ascended onto the scarlet pedestal and towered over the London square."), /*#__PURE__*/React.createElement("div", {
     className: "flex justify-content gap-8"
@@ -81,10 +81,10 @@ function App(props) {
     src: "./assets/OratorSpeech.png"
   }), /*#__PURE__*/React.createElement("div", null, "\u201COur forces in South India have won yet again in a ruthless battle against the ", warTarget + "n", " forces. Our borders have expanded immensely and we will continue to push into ", warTarget + "n", " territory and claim the land that belongs to us! This war against Eastasia will soon come to an end, Comrades!\u201D - The Orator")), /*#__PURE__*/React.createElement("div", {
     className: "subtitle"
-  }, " Suddenly, the glorious celebration of hatred that was happening quickly turned into a disaster."), /*#__PURE__*/React.createElement("div", {
+  }, " Suddenly, the glorious celebration of hatred that was happening quickly turned into a plusungood disaster."), /*#__PURE__*/React.createElement("div", {
     className: "flex justify-content gap-8",
     ref: warSwapRef
-  }, /*#__PURE__*/React.createElement("div", null, "All the banners around the square had been sabotaged and were decorated all wrong. The horrendous scarlet banners, which were supposed to have the faces of our Eastasian enemies, have been replaced by the faces of our Eurasian allies."), /*#__PURE__*/React.createElement("div", null, "Countless insider reports have come to the conclusion that this was the work of Goldstein\u2019s sinister agents. Many well-trained Junior spies effortlessly climbed over rooftops cutting down streamers and taking down banners."), /*#__PURE__*/React.createElement("div", null, "Good samaritans all around the square quickly went to work destroying the sabotaged banners, tearing down the Goldstein propaganda instantly. Once all of Goldstein\u2019s revolting banners were abolished and torn down, the orator concluded his grand speech and walked off the stage. The war against Eastasia continues, but Oceania, alongside our Eurasian allies, will not back down.")));
+  }, /*#__PURE__*/React.createElement("div", null, "All the banners around the square had been sabotaged and were decorated all wrong. The horrendous scarlet banners, which were supposed to have the faces of our Eastasian enemies, have been replaced by the faces of our Eurasian allies."), /*#__PURE__*/React.createElement("div", null, "Countless insider reports have come to the conclusion that this was the work of Goldstein\u2019s sinister agents. Many goodwise trained Junior Spies effortlessly climbed over rooftops cutting down streamers and taking down banners."), /*#__PURE__*/React.createElement("div", null, "Plusgood samaritans all around the square quickly went to work destroying the sabotaged banners, tearing down the Goldstein propaganda instantly. Once all of Goldstein\u2019s revolting banners were abolished and torn down, the orator concluded his grand speech and walked off the stage. The war against Eastasia continues, but Oceania, alongside our Eurasian allies, will not back down.")));
   const random = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -136,55 +136,65 @@ function App(props) {
     pages: pages
   })), /*#__PURE__*/React.createElement("div", {
     id: "control",
-    className: "box-border m-4 w-1/5 fixed h-full right-0 top-0 bg-[#2E2C2D] border-[#C4C2C3] border-x-[15px] p-[15px] outline-[15px] flex flex-col justify-around"
+    className: "w-1/5 absolute h-full right-0 top-0 bg-[#2E2C2D] border-[#4d4d4d] border-x-[20px] p-[15px] outline-[15px] flex flex-col justify-around"
   }, /*#__PURE__*/React.createElement("div", {
     className: "h-64 text-white flex justify-center"
   }, /*#__PURE__*/React.createElement("div", {
-    className: "rounded-full bg-slate-900 w-60 h-60 flex flex-col justify-around items-center"
+    className: "rounded-full bg-zinc-900 w-60 h-60 flex flex-col justify-around items-center"
   }, /*#__PURE__*/React.createElement("div", {
     className: "flex justify-around items-center"
   }, /*#__PURE__*/React.createElement("div", {
     className: "h-48 w-28 flex justify-around items-center",
     onClick: () => changePage(-1)
   }, /*#__PURE__*/React.createElement("div", {
-    className: "text-slate-600 w-0 h-0 border-y-[40px] border-y-slate-900 border-r-[80px] border-r-slate-600 hover:border-r-slate-800 duration-150 " + (curPage == 0 ? "border-r-slate-900" : "")
+    className: "text-slate-600 w-0 h-0 border-y-[40px] border-y-zinc-900 border-r-[80px] border-r-slate-600 duration-150 " + (curPage == 0 ? "border-r-zinc-900" : "hover:border-r-slate-800")
   })), /*#__PURE__*/React.createElement("div", {
     className: "h-48 w-28 flex justify-around items-center",
     onClick: () => changePage(1)
   }, /*#__PURE__*/React.createElement("div", {
-    className: "text-slate-600 w-0 h-0 border-y-[40px] border-y-slate-900 border-l-[80px] border-l-slate-600 hover:border-l-slate-800 duration-150 " + (curPage == 2 ? "border-l-slate-900" : "")
+    className: "text-slate-600 w-0 h-0 border-y-[40px] border-y-zinc-900 border-l-[80px] border-l-slate-600 duration-150 " + (curPage == 2 ? "border-l-zinc-900" : "hover:border-l-slate-800")
   }))), /*#__PURE__*/React.createElement("div", {
     className: "w-full h-8 flex justify-center text-white"
   }, curPage + 1, "/3"))), /*#__PURE__*/React.createElement("div", {
-    className: "h-2/5 flex flex-col justify-around"
+    className: "h-1/2 flex flex-col justify-around"
   }, /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
-    className: "w-full h-2 bg-slate-900 border-slate-200 border-t-2"
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "w-full h-2 bg-slate-900 border-slate-400 border-t-2"
   }), /*#__PURE__*/React.createElement("div", {
     className: "w-full flex justify-center text-slate-400"
   }, "INGSOC"))));
